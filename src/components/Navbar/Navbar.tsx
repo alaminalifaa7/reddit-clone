2171c327a5919bb4f9ff62a236593f92bff2e2f9
alif <alaminalifaa7@gmail.com>
2023-02-01 17:17:59 +0600

    text change in the navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 const Navbar:React.FC = () => {
     
-    return <div>Navbar is working even in git</div>
+    return <div>Navbar is working trying to change from vsc od</div>
 }
 export default Navbar;
