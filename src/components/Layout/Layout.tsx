8c20f496046d3891dd61de83d6e9ee5f2db55b22
alif <alaminalifaa7@gmail.com>
2023-02-01 16:32:26 +0600

    committing reddit clone structure is ready

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import Navbar from '../Navbar/Navbar';
+
+//we create types for each component and 
+//this types specify what props needs to passed to this component
+type Props = {
+    children?: React.ReactNode
+  };
+const Layout:React.FC<Props> = ({children}) => {
+    
+    return (
+    <>
+        <Navbar />
+        <main>{children}</main>
+
+    </>
+    );
+};
+export default Layout;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Navbar:React.FC = () => {
+    
+    return <div>Navbar is working even in git</div>
+}
+export default Navbar;
