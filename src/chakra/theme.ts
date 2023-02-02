291cc037044f037eb55e1db53f7b277fcee28a0c
alif <alaminalifaa7@gmail.com>
2023-02-02 16:40:46 +0600

    changed button style with a global style

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -3,6 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
+import {Button} from "./button";
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
   colors: {
@@ -21,6 +22,6 @@ export const theme = extendTheme({
     })
   },
   components:{
-  //  Button
+    Button,
   }
 })
