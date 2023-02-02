0cdfc13570d06ada05e56f6570e1bad964f6a7a7
alif <alaminalifaa7@gmail.com>
2023-02-02 15:23:27 +0600

    navbar search input and right content buttons added

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import Navbar from '../Navbar/Navbar';
 
 //we create types for each component and 
-//this types specify what props needs to passed to this component
+//this types specify what props needs to be passed to this component
 type Props = {
     children?: React.ReactNode
   };
---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import AuthButtons from "./AuthButtons";
+
+type RightContentProps = {
+  //user:any;
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+  return (
+    <>
+      <Flex justify="center" align="center">
+        <AuthButtons />
+      </Flex>
+    </>
+  );
+};
+export default RightContent;
