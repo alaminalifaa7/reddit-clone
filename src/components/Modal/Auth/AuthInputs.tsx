f594629918f493e01c15c99863116283a9149583
alif <alaminalifaa7@gmail.com>
2023-02-06 16:40:34 +0600

    added authinput and login component

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type AuthInputsProps = {};
+
+const AuthInputs: React.FC<AuthInputsProps> = () => {
+  return <Flex direction="column" align="center" width="100%" mt={4}></Flex>;
+};
+export default AuthInputs;
